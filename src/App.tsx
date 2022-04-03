44b0139e86870670792c990297e61c021fa02335
wael-fadlallah <wael.fudlallah@gmail.com>
2022-04-03 22:36:36 +0400

    added the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from "./Navbar/Index";
+
 function App() {
-  return <div className="text-3xl font-bold underline">test</div>;
+  return (
+    <div className="text-3xl font-bold underline">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
