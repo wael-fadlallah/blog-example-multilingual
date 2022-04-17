e90aad9f13c633e76324558e8f73062b87395f4b
wael-fadlallah <wael.fudlallah@gmail.com>
2022-04-17 23:55:29 +0400

    link i18next with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import Navbar from "./components/Navbar/Index";
 import Footer from "./components/Footer/Index";
 import Card from "./components/Card/Index";
-
+import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 function App() {
+  const { t, i18n } = useTranslation();
+  const user = useSelector((state: any) => state.user);
+
+  useEffect(() => {
+    i18n.changeLanguage(user.language);
+  }, [user]);
   return (
     <>
       <div className="content">
